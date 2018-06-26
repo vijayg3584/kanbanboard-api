720a5c25d8e3bf374f9c9985dda4e06ee41b9fb7
Vijay Ganesh <vijay@trueinfluence.in>
2018-06-26 23:45:18 +1000

    Initial commit

---
+++ src/main/java/com/partnersincode/kanbanboardapi/KanbanboardApiApplication.java
@@ -0,0 +1,12 @@
+package com.partnersincode.kanbanboardapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanboardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanboardApiApplication.class, args);
+	}
+}
